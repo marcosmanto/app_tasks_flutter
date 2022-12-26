904eb710942ee7ce2375a7762c53896b95c71258
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-26 17:55:30 -0300

    Added flutter_slidable to show delete action

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import '../utils/date_time_extension.dart';
 
 import '../models/todo.dart';
@@ -9,48 +10,83 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //height: 60,
-      margin: const EdgeInsets.only(bottom: 8),
-      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
+    return Column(
+      children: [
+        Slidable(
+          key: const ValueKey(0),
+          endActionPane: ActionPane(
+            extentRatio: .3,
+            motion: ScrollMotion(),
             children: [
-              Icon(
-                Icons.calendar_today_outlined,
-                size: 12,
-                color: Colors.grey[600],
+              // A SlidableAction can have an icon and/or a label.
+              SlidableAction(
+                onPressed: null,
+                backgroundColor: Colors.transparent,
+                foregroundColor: Colors.transparent,
+                icon: null,
+                label: '',
               ),
-              SizedBox(
-                width: 4,
-              ),
-              Text(
-                todo.createdAt
-                    .format('dd/MM/yyyy, HH:mm - EEEE', 'pt-BR')
-                    .toUpperCase(),
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.w300,
-                  letterSpacing: .75,
-                ),
+              SlidableAction(
+                flex: 12,
+                spacing: 5,
+                onPressed: null,
+                borderRadius: BorderRadius.all(Radius.circular(8)),
+                backgroundColor: Color(0xFFFE4A49),
+                foregroundColor: Colors.white,
+                icon: Icons.delete,
+                label: 'Excluir',
               ),
             ],
           ),
-          Text(
-            todo.title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+          child: Container(
+            //height: 120,
+            //margin: const EdgeInsets.only(bottom: 8),
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_today_outlined,
+                      size: 12,
+                      color: Colors.grey[600],
+                    ),
+                    SizedBox(
+                      width: 4,
+                    ),
+                    Text(
+                      todo.createdAt
+                          .format('dd/MM/yyyy, HH:mm - EEEE', 'pt-BR')
+                          .toUpperCase(),
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w300,
+                        letterSpacing: .75,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 6),
+                  child: Text(
+                    todo.title,
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        ),
+        const SizedBox(height: 8),
+      ],
     );
   }
 }
