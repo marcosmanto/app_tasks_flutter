b6c10ca67bac95fd9f1d23f25621b84a2db18dc7
Marcos Mantovani <mantovani.marcos@gmail.com>
2022-12-27 12:37:45 -0300

    Set window size for Windows platform using desktop_windows package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
-import 'package:app_tasks_flutter/pages/todo_list_page.dart';
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:app_tasks_flutter/pages/todo_list_page.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:desktop_window/desktop_window.dart';
+
+void main() async {
+  if (!kIsWeb && Platform.isWindows) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await DesktopWindow.setWindowSize(Size(495, 730));
+    await DesktopWindow.setMinWindowSize(Size(495, 730));
+    await DesktopWindow.setMaxWindowSize(Size(730, 880));
+  }
   runApp(MyApp());
 }
 
